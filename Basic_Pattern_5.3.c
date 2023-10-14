f0401a533e6234f875c9624e2d9833cd245dcd10
BhargavaKandala <144446705+BhargavaKandala@users.noreply.github.com>
2023-10-14 06:12:22 +0530

    #include <stdio.h> int main() {     int n;     scanf("%d",&n);
        for(int i=n;i>=1;i--){
            for(int j=1;j<=n;j++){
                if(j<=i){
                    printf("%d",i);
                }
                else printf("*");
            }
            printf("
    ");
        }
    }

---
+++ Basic_Pattern_5.3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=n;j++){
+            if(j<=i){
+                printf("%d",i);
+            }
+            else printf("*");
+        }
+        printf("
+");
+    }
+}
