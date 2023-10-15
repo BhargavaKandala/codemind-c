afe532099990ead61868c8b0c1fdfe9fea616596
BhargavaKandala <144446705+BhargavaKandala@users.noreply.github.com>
2023-10-15 08:02:19 +0530

    #include <stdio.h> int main() {     int n,i,j;     scanf("%d",&n);
       for (int i=n;i>=1;i--){
            for(int j=n;j>=1;j--){
               if(i<j) 
    		    printf(" ");
    		   
              else printf("%d ",i);
            }
            printf("
    ");
        }
    }

---
+++ Pattern_7.2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    
+   for (int i=n;i>=1;i--){
+        for(int j=n;j>=1;j--){
+           if(i<j) 
+		    printf(" ");
+		   
+          else printf("%d ",i);
+        }
+        printf("
+");
+    }
+}
