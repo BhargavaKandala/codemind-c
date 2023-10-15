c722fb293a1d339b896f2c7568e3c6dc3102cf32
BhargavaKandala <144446705+BhargavaKandala@users.noreply.github.com>
2023-10-15 09:05:40 +0530

    #include <stdio.h> int main() {     int n;     scanf("%d",&n);
        for(int i=n;i>=1;i--){
            for(int j=1;j<=n;j++){
                if(i==j || i==n-j+1)
                 printf("%d",i);
                else printf(" ");
            }
            printf("
    ");
        }
    }

---
+++ Pattern_8.3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=n;j++){
+            if(i==j || i==n-j+1)
+             printf("%d",i);
+            else printf(" ");
+        }
+        printf("
+");
+    }
+}
