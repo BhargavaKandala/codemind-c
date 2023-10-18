d7ac51d27e08bbf1b3821f489fedc6de1b9d2c07
BhargavaKandala <144446705+BhargavaKandala@users.noreply.github.com>
2023-10-18 10:09:27 +0530

    #include <stdio.h> int main() {     int n;     scanf("%d",&n);
        if(n%4==0){
            printf("%d",n/4);
        }
        else if(n%4!=0){
            printf("%d",(n/4)+1);
        }
    }

---
+++ Cars.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    
+    if(n%4==0){
+        printf("%d",n/4);
+    }
+    else if(n%4!=0){
+        printf("%d",(n/4)+1);
+    }
+}
