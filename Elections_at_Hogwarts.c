9ae30b44bfc35a6bf5a92531c2de93cfc460ea5e
BhargavaKandala <144446705+BhargavaKandala@users.noreply.github.com>
2023-10-27 08:48:58 +0530

    #include <stdio.h> int main() {     int x,y,z;     scanf("%d%d%d",&x,&y,&z);     if(x>50)     printf("Gryffindor");     else if(y>50)     printf("Slytherin");     else if(z>50)     printf("Hufflepuff");     else printf("NOTA"); }

---
+++ Elections_at_Hogwarts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int x,y,z;
+    scanf("%d%d%d",&x,&y,&z);
+    if(x>50)
+    printf("Gryffindor");
+    else if(y>50)
+    printf("Slytherin");
+    else if(z>50)
+    printf("Hufflepuff");
+    else printf("NOTA");
+}
