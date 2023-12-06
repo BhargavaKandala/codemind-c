22c11ff4e28be535b7b748c5fbaa9453ee054382
BhargavaKandala <144446705+BhargavaKandala@users.noreply.github.com>
2023-12-06 16:58:28 +0530

    #include<stdio.h> int main() {     int n,sum = 0 , count = 0;     scanf("%d",&n);
        int arr[n];
        for(int i = 0; i < n; i++){
            scanf("%d",&arr[i]);
        }
        
        for(int i = 0; i < n; i++){
            sum += arr[i];
        }
        int result = sum /n;
        
        for(int i = 0; i < n;i++){
            if(arr[i] <= result){
                count++;
            }
        }
        printf("%d",count);
        
    }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum = 0 , count = 0;
+    scanf("%d",&n);
+    
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d",&arr[i]);
+    }
+    
+    for(int i = 0; i < n; i++){
+        sum += arr[i];
+    }
+    int result = sum /n;
+    
+    for(int i = 0; i < n;i++){
+        if(arr[i] <= result){
+            count++;
+        }
+    }
+    printf("%d",count);
+    
+}
