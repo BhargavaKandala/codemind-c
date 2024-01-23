cfe2a051ce887076da30c56ec71586d7b47e82d8
BhargavaKandala <144446705+BhargavaKandala@users.noreply.github.com>
2024-01-23 11:30:32 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d", &n);     int arr[n];     for(int i = 0; i < n; i++){         scanf("%d", &arr[i]);     }
        int target;
        scanf("%d",&target);
        int found = 1;
        
        
         for(int i = 0; i < n; i++){
             int count = 0;
              for(int j = 0; j < n; j++){
                  if(arr[i] == arr[j]){
                      count++;
                  }
              }
              if(count == target){
                  printf("%d ",arr[i]);
                  arr[i] = 0;
                  found = 0;
              }
         }
        if(found){
            printf("-1");
        }
    }

---
+++ N_times_repeated_Elements.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    
+    int target;
+    scanf("%d",&target);
+    int found = 1;
+    
+    
+     for(int i = 0; i < n; i++){
+         int count = 0;
+          for(int j = 0; j < n; j++){
+              if(arr[i] == arr[j]){
+                  count++;
+              }
+          }
+          if(count == target){
+              printf("%d ",arr[i]);
+              arr[i] = 0;
+              found = 0;
+          }
+     }
+    if(found){
+        printf("-1");
+    }
+}
+
