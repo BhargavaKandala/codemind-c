b7baa4aa09545d86e5f2e735832406bf6c9d9328
BhargavaKandala <144446705+BhargavaKandala@users.noreply.github.com>
2024-01-26 22:14:12 +0530

    #include<stdio.h> int odd(int num){     if(num % 2 == 1){         return 1;     }     return 0; }
    int main()
    {
        int n , m;
        scanf("%d %d" , &n, &m);
        int arr[n][m];
        for(int i = 0; i < n; i++){
            for(int j = 0; j < m; j++){
                scanf("%d",&arr[i][j]);
            }
        }
        
        int oddsum = 0;
        int evensum = 0;
        for(int i = 0; i < n; i++){
            for(int j = 0; j < m; j++){
                if(odd(arr[i][j])){
                    oddsum += arr[i][j];
                }
                else evensum += arr[i][j];
            }
        }
        printf("%d %d", evensum , oddsum);
    }

--- Sum_of_even_and__odd_elements_in_a_matrix.c
+++ Sum_of_even_and__odd_elements_in_a_matrix.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
+int odd(int num){
+    if(num % 2 == 1){
+        return 1;
+    }
+    return 0;
+}
+
 int main()
 {
-    int n,m;
-    int evencount = 0, oddcount = 0;
-    scanf("%d%d",&n, &m);
-    
+    int n , m;
+    scanf("%d %d" , &n, &m);
     int arr[n][m];
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
@@ -12,15 +17,15 @@ int main()
         }
     }
     
+    int oddsum = 0;
+    int evensum = 0;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
-          if(arr[i][j] % 2 == 0){
-             evencount = evencount + arr[i][j];
-        }
-          if(arr[i][j] % 2 == 1){
-             oddcount = oddcount + arr[i][j];
+            if(odd(arr[i][j])){
+                oddsum += arr[i][j];
+            }
+            else evensum += arr[i][j];
         }
     }
-  }
-  printf("%d %d",evencount , oddcount);
+    printf("%d %d", evensum , oddsum);
 }
