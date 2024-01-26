b5123b649857973b49f0c87d0445cdb2cd37524f
BhargavaKandala <144446705+BhargavaKandala@users.noreply.github.com>
2024-01-26 22:07:33 +0530

    #include<stdio.h> int main() {     int n , m , flag = 0;     scanf("%d %d ", &n, &m);     int arr[n][m];
        for(int i = 0; i < n; i++){
            for(int j = 0; j < m; j++){
                scanf("%d ", &arr[i][j]);
            }
        }
        
        int num;
        scanf("%d", &num);
        
        for(int i = 0; i < n; i++){
            for(int j = 0; j < m; j++){
                if(num == arr[i][j]){
                    flag = 1;
                }
            }
        }
        
           printf("%d", flag);
    }

--- Search_in_a_matrix.c
+++ Search_in_a_matrix.c
@@ -1,25 +1,26 @@
 #include<stdio.h>
 int main()
 {
-    int n, m, flag = 0;
-    scanf("%d %d",&n, &m);
-    
+    int n , m , flag = 0;
+    scanf("%d %d ", &n, &m);
     int arr[n][m];
-    for(int i = 0 ; i < n ; i++){
-        for(int j = 0; j < m ; j++){
-            scanf("%d",&arr[i][j]);
+    
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            scanf("%d ", &arr[i][j]);
         }
     }
     
     int num;
-    scanf("%d",&num);
+    scanf("%d", &num);
     
-    for(int i = 0 ;i < n ; i++){
-        for(int j = 0; j < m ; j++){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
             if(num == arr[i][j]){
                 flag = 1;
             }
         }
     }
-    printf("%d",flag);
+    
+       printf("%d", flag);
 }
