b5b278c4caca0eb2ec8df18ed7382f9a331c5d0b
BhargavaKandala <144446705+BhargavaKandala@users.noreply.github.com>
2024-01-26 21:50:13 +0530

    #include<stdio.h> int main() {     int n,m;     scanf("%d %d",&n,&m);     int arr[n][m];
        for(int i = 0; i < n; i++){
            for(int j = 0; j < m; j++){
                scanf("%d",&arr[i][j]);
            }
        }
        
        int a, b;
        scanf("%d %d",&a,&b);
        int arr2[a][b];
        for(int i = 0 ;i < a; i++){
            for(int j = 0; j < b; j++){
                scanf("%d",&arr2[i][j]);
            }
        }
        
        
        for(int i = 0; i < n; i++){
            for(int j = 0; j < m; j++){
                printf("%d ", arr[i][j] + arr2[i][j]);
            }
            printf("
    ");
        }
        
    }

--- Matrix_Addition.c
+++ Matrix_Addition.c
@@ -1,34 +1,32 @@
 #include<stdio.h>
 int main()
 {
-    int n , m;
-    scanf("%d %d" , &n, &m);
-    
+    int n,m;
+    scanf("%d %d",&n,&m);
     int arr[n][m];
+    
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
-            scanf("%d" ,&arr[i][j]);
+            scanf("%d",&arr[i][j]);
         }
     }
     
-    int a ,b;
-    scanf("%d %d" , &a, &b);
-    
+    int a, b;
+    scanf("%d %d",&a,&b);
     int arr2[a][b];
-    for(int i = 0; i < a; i++){
+    for(int i = 0 ;i < a; i++){
         for(int j = 0; j < b; j++){
-            scanf("%d" ,&arr2[i][j]);
+            scanf("%d",&arr2[i][j]);
         }
     }
     
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<m; j++){
-            printf("%d ", (arr[i][j] + arr2[i][j]));
+    
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            printf("%d ", arr[i][j] + arr2[i][j]);
         }
-        
         printf("
 ");
     }
-    
     
 }
