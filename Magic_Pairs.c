77223991d7ea94f27f755feab7f5fb6706710903
BhargavaKandala <144446705+BhargavaKandala@users.noreply.github.com>
2024-02-03 16:08:47 +0530

    #include<stdio.h> int is_odd(int num){     if(num % 2 == 1){         return 1;     }     return 0; } int main() {     int n;     scanf("%d",&n);     int count = 0;     for(int i = 1; i <= n; i++){         for(int j = 1; j <= n; j++){             if(is_odd((i + j))){                 count++;             }         }     }     printf("%d",count); }

---
+++ Magic_Pairs.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int is_odd(int num){
+    if(num % 2 == 1){
+        return 1;
+    }
+    return 0;
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int count = 0;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(is_odd((i + j))){
+                count++;
+            }
+        }
+    }
+    printf("%d",count);
+}
