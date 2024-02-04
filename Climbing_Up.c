96bb4d857b00a7896d7784a5b2cd854b8408a1f8
BhargavaKandala <144446705+BhargavaKandala@users.noreply.github.com>
2024-02-04 10:27:57 +0530

    #include<stdio.h> int main() {     int x,y;     scanf("%d %d", &x,&y);     if(x % y == 0)         printf("%d",y);         else printf("%d", x % y + x / y); }

--- Climbing_Up.c
+++ Climbing_Up.c
@@ -1,13 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
     int x,y;
-    scanf("%d%d",&x,&y);
-    
-    if(x%y==0)
-    printf("%d",y);
-    else 
-    printf("%d",x/y+x%y);
-    
-    
+    scanf("%d %d", &x,&y);
+    if(x % y == 0)
+        printf("%d",y);
+        else printf("%d", x % y + x / y);
 }
