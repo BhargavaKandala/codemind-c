24b1fdd951b7a96bd883ce74143db9bf9ef26abf
BhargavaKandala <144446705+BhargavaKandala@users.noreply.github.com>
2024-02-08 16:47:34 +0530

    #include<stdio.h> int main() {     int n,m;     scanf("%d %d", &n, &m);
        int arr[n][m];
        
        for(int i = 0; i < n; i++){
            for(int j = 0; j < m; j++){
                scanf("%d",&arr[i][j]);    
            }
        }
        
        int rowsum[n] = {0} ,colsum[m] = {0};
        int maxrow = 0 , maxcol = 0;
        
        for(int i = 0; i < n; i++){
            for(int j = 0; j < m; j++){
                rowsum[i] += arr[i][j];
                colsum[j] += arr[i][j];
            }
        }
        
          for(int i = 0; i < n; i++){
              if(rowsum[i] > maxrow){
                  maxrow = rowsum[i];
              }
          }
          
        
          for(int j = 0; j < n; j++){
              if(colsum[j] > maxcol){
                  maxcol = colsum[j];
              }
          }
          
          if(maxcol > maxrow){
              printf("%d ", maxcol);
          }
          else printf("%d ",maxrow);
          
    }

--- Maximum_sum_of_row_wise_sum_or_column_wise_sum_of_matrix.c
+++ Maximum_sum_of_row_wise_sum_or_column_wise_sum_of_matrix.c
@@ -1,37 +1,46 @@
 #include<stdio.h>
 int main()
 {
-    int n ,m;
-    scanf("%d %d",&n, &m);
+    int n,m;
+    scanf("%d %d", &n, &m);
+    
     int arr[n][m];
+    
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
-            scanf("%d", &arr[i][j]);
+            scanf("%d",&arr[i][j]);    
         }
     }
     
-    int row[n] = {0}, col[m] = {0};
-    int max_row = 0 , max_col = 0;
-    
-    
-     for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            row[i] += arr[i][j];
-            col[j] += arr[i][j];
-        }
-     }
+    int rowsum[n] = {0} ,colsum[m] = {0};
+    int maxrow = 0 , maxcol = 0;
     
     for(int i = 0; i < n; i++){
-        max_row =(row[i] > max_row) ? row[i] : max_row;
+        for(int j = 0; j < m; j++){
+            rowsum[i] += arr[i][j];
+            colsum[j] += arr[i][j];
+        }
     }
     
-    for(int j = 0; j < m; j++){
-        max_col =(col[j] > max_col) ? col[j] : max_col;
-    }
-
-    if(max_row > max_col){
-        printf("%d ", max_row);
-    }
-    else printf("%d ", max_col);
+      for(int i = 0; i < n; i++){
+          if(rowsum[i] > maxrow){
+              maxrow = rowsum[i];
+          }
+      }
+      
     
+      for(int j = 0; j < n; j++){
+          if(colsum[j] > maxcol){
+              maxcol = colsum[j];
+          }
+      }
+      
+      if(maxcol > maxrow){
+          printf("%d ", maxcol);
+      }
+      else printf("%d ",maxrow);
+      
 }
+
+
+ 
